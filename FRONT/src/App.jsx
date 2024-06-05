ef4b91f11575b3235a4d89d12aa9cb7377976571
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-06-05 15:24:46 +0200

    Mise en place de la base  header-routers etc...

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './components/layout';
+import Home from './pages/home/Home';
+
+
+
 
 function App() {
 
-
   return (
-    <>
-     <h1>projet X</h1>
-    </>
+  <BrowserRouter>
+    <Layout>
+      <Routes>
+        <Route index path="/" element={<Home/>}/>
+      </Routes>
+    </Layout>
+  </BrowserRouter>
   )
 }
 
