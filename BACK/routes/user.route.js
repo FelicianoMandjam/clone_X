49b712aa294f0f432c0a7012d9237e660dbcc6f8
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-06-10 10:56:39 +0200

    ajout de la connexion , sign-up BACK

--- BACK/routes/user.route.js
+++ BACK/routes/user.route.js
@@ -3,7 +3,7 @@ import * as userController from '../controllers/user.controller.js';
 
 const router = express.Router()
 
-router.get('/', userController.get)
+router.get('/sign', userController.login)
 router.post("/add", userController.post)
 // router.put("/put", userController.put)
 // router.delete("/delete", userController.delet)
