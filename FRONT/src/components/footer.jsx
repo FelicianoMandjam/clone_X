fa36929ae4f70e776415ded11d3f9d813ed4032c
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-06-11 23:13:43 +0200

    Ajustment de la page HOME

--- FRONT/src/components/footer.jsx
+++ FRONT/src/components/footer.jsx
@@ -3,83 +3,16 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
-import { NavLink } from "react-router-dom";
-
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Login from "../pages/Auth/Login.jsx";
-
-
 import { AuthContext } from "../../context/AuthContext.jsx";
 
 function Footer() {
-  const [show, setShow] = useState(false);
-  const [user, setUser] = useState({})
-  const { login } = useContext(AuthContext)
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  const handleChange = (event) => {
-    const { name, value } = event.target
-    setUser((user) => ({ ...user, [name]: value }))    
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    // console.log("FOO +=>")
-    login(user)
-  }
-
 
   
   return (
     <>
-      <div className="footer position-fixed w-100 d-flex justify-content-around bg-primary" >
-        <div className="text mt-2">
-          <h2>Ne manquez pas ce qui se passe.</h2>
-          <p>Les utilisateurs de X sont les premiers à savoir.</p>
-        </div>
-        <div className="btn mt-2">
-          {/* Se connecter */}
-          <Button className="border-white rounded-pill" onClick={handleShow}>
-            Se connecter
-          </Button>
-
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header className="bg-dark" closeButton>
-              <Modal.Title className="bg-dark">Se connecter</Modal.Title>
-            </Modal.Header>
-            <Modal.Body className="bg-dark">
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formGroupEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" name="email" placeholder="Enter email" onChange={handleChange} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formGroupPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" name="password" placeholder="Password" onChange={handleChange} />
-                  <Button className="w-75" type="submit">
-                Continuer
-              </Button>
-                </Form.Group>
-              </Form>
-            </Modal.Body>
-            <Modal.Footer className="bg-dark d-flex justify-content-center">
-         
-            </Modal.Footer>
-          </Modal>
-
-          <Button className="bg-white text-dark rounded-pill" href="/register">
-            S'inscrire
-          </Button>
-          
-        </div>
-      </div>
     </>
   );
 }
 
 export default Footer;
-
 
--- FRONT/src/pages/home/Home.jsx
+++ FRONT/src/pages/home/Home.jsx
@@ -1,34 +1,88 @@
 import React from 'react';
 import Login from '../Auth/Login';
 import '../../style/home.css';
+import { useState , useContext } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+import { AuthContext } from '../../../context/AuthContext.jsx';
 
 const Home = () => {
-  return (
-    <div className="home">
-      <div className="home-header">
+  const [show, setShow] = useState(false);
+  const [user, setUser] = useState({})
+  const { login } = useContext(AuthContext)
 
-        <h1>BIenvenue sur Project X</h1>
-        <p>Connectez-vous avec vos amis et le monde qui vous entoure sur Project X.</p>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setUser((user) => ({ ...user, [name]: value }))    
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log("FOO +=>")
+    login(user)
+  }
+
+
+  return (
+    <div className=' text-white'>
+     <div className="content">
+     <h1>Bienvenue sur notre site!</h1>
+     <p>Connectez-vous pour découvrir plus.</p>
       </div>
-      <div className="home-content">
-        <div className="home-tweets">
-          <div className="tweet">
-            <p>C'est comment les gars ?</p>
-            <p>@Feliciano</p>
-          </div>
-          <div className="tweet">
-            <p>Yahn envoie ton casque</p>
-            <p>@Julien</p>
-          </div>
-          <div className="tweet">
-            <p>Francis il est dans le métro</p>
-            <p>@Ghani</p>
-          </div>
-         
-        </div>
+
+      {localStorage.length < 1 && (
+    <div className="footer position-fixed w-100 d-flex justify-content-around bg-primary">
+      <div className="text mt-2">
+        <h2>Ne manquez pas ce qui se passe.</h2>
+        <p>Les utilisateurs de X sont les premiers à savoir.</p>
       </div>
-    </div>
-  );
+
+     
+      <div className="btn mt-2">
+        <Button className="border-white rounded-pill" onClick={handleShow}>
+          Se connecter
+        </Button>
+
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header className="bg-dark" closeButton>
+            <Modal.Title className="bg-dark">Se connecter</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body className="bg-dark">
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter email" onChange={handleChange} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Password" onChange={handleChange} />
+                <Button className="w-75 mt-3" type="submit" onClick={handleClose}>Continuer</Button>
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+
+          <Modal.Footer className="bg-dark d-flex justify-content-center">
+            <h3>Merci</h3>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div> )}
+
+    {localStorage.length > 0 && (
+      <div className='content'>
+        <h1>Tweet du jour:</h1>
+        <input type="text" />
+        <label htmlFor="tweet"/>
+      </div>
+    )}
+  </div>
+);
 };
 
 export default Home;
